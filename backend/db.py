0122a816b0128a94efdddd540d218ecad84fd202
Sanket Jadhav <sanket25041@gmail.com>
2026-06-25 09:47:03 +0530

    new

--- backend/db.py
+++ backend/db.py
@@ -10,7 +10,8 @@ def get_connection():
                 host=os.getenv("DB_HOST"),
                 user=os.getenv("DB_USER"),
                 password=os.getenv("DB_PASSWORD"),
-                database=os.getenv("DB_NAME")
+                database=os.getenv("DB_NAME"),
+                connection_timeout=5
             )
             print("Connected to MySQL successfully")
             return conn
